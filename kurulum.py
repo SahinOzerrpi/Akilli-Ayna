223ce801e6d48e4ae1065719982b6b73c2bf9fff
SahinOzerrpi <94168902+SahinOzerrpi@users.noreply.github.com>
2022-01-13 02:08:53 +0300

    Add files via upload

--- kurulum.py
+++ kurulum.py
@@ -2,23 +2,23 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-# Must be ran as root or as sudo
-if os.getuid() != 0:
+# Root veya sudo olarak çalıştırılmalıdır
+if os.getpid() != 0:
     print('HATA: Root veya sudo olarak çalıştırmanız gerekiyor')
     sys.exit(1)
 
-# Install the requirements if the system does not have it installed
+# Sistemde kurulu değilse gereksinimleri yükleyin
 print('BİLGİ: Gerekenleri kontrol et ve yükle')
 os.system('! dpkg -S python-imaging-tk && apt-get -y install python-imaging-tk')
 
-# Generate the requirements from the file for old instructions
+# Gerekli kütüphaneler için dosyadan gereksinimleri oluşturun
 print('BİLGİ: Gerekenler.txt dosyasından indirilecek kütüphaneler vardır')
 packages = []
 for line in open('gerekenler.txt', 'r'):
     if not line.startswith('#'):
         packages.append(line.strip())
 
-# Run setuptools for pip
+# pip için setuptools'u çalıştırın
 setup(
     name='akilliayna',
     version='1.0',
